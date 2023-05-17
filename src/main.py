4160263fffbc7787458757361b204208c4d97400
jdfrancisco <jdavidfrancisco@gmail.com>
2023-05-16 21:24:12 -0700

    Finished FGSM attack

--- src/main.py
+++ src/main.py
@@ -38,6 +38,15 @@ def preprocess(image):
 def get_imagenet_label(probs):
   return decode_predictions(probs, top=1)[0][0]
 
+def display_images(image, description):
+  _, label, confidence = get_imagenet_label(pretrained_model.predict(image))
+  plt.figure()
+  plt.imshow(image[0]*0.5+0.5)
+  plt.title('{} \n {} : {:.2f}% Confidence'.format(description, label, confidence*100))
+  plt.show()
+
+print("-- Start executing FGSM Attack ---")
+
 def create_adversarial_pattern(input_image, input_label):
   with tf.GradientTape() as tape:
     tape.watch(input_image)
@@ -50,19 +59,10 @@ def create_adversarial_pattern(input_image, input_label):
   signed_grad = tf.sign(gradient)
   return signed_grad
 
-def display_images(image, description):
-  _, label, confidence = get_imagenet_label(pretrained_model.predict(image))
-  plt.figure()
-  plt.imshow(image[0]*0.5+0.5)
-  plt.title('{} \n {} : {:.2f}% Confidence'.format(description,
-                                                   label, confidence*100))
-  plt.show()
-
 mpl.rcParams['figure.figsize'] = (8, 8)
 mpl.rcParams['axes.grid'] = False
 
-pretrained_model = tf.keras.applications.MobileNetV2(include_top=True,
-                                                     weights='imagenet')
+pretrained_model = tf.keras.applications.MobileNetV2(include_top=True, weights='imagenet')
 pretrained_model.trainable = False
 
 # ImageNet labels
@@ -84,15 +84,15 @@ plt.title('{} : {:.2f}% Confidence'.format(image_class, class_confidence*100))
 loss_object = tf.keras.losses.CategoricalCrossentropy()
 
 # Get the input label of the image.
-labrador_retriever_index = 208
-label = tf.one_hot(labrador_retriever_index, image_probs.shape[-1])
+animal_index = 208
+label = tf.one_hot(animal_index, image_probs.shape[-1])
 label = tf.reshape(label, (1, image_probs.shape[-1]))
 
 perturbations = create_adversarial_pattern(image, label)
 plt.imshow(perturbations[0] * 0.5 + 0.5);  # To change [-1, 1] to [0,1]
 # plt.show()
 
-epsilons = [0, 0.01, 0.1, 0.15]
+epsilons = [0, 0.01, 0.1, 0.15, 0.2, 0.25]
 descriptions = [('Epsilon = {:0.3f}'.format(eps) if eps else 'Input')
                 for eps in epsilons]
 
@@ -101,4 +101,4 @@ for i, eps in enumerate(epsilons):
   adv_x = tf.clip_by_value(adv_x, -1, 1)
   display_images(adv_x, descriptions[i])
 
-print("-- Done executing functions ---")
+print("-- End executing FGSM Attack ---")
